e73b137b93f535c45badeb70184f00b9825b09ed
shauryajain2067 <77152865+shauryajain2067@users.noreply.github.com>
2022-05-21 12:51:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(300,300);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+}
+
+function draw(){
+image(video,0,0,300,300)
+}
+
+function take_snapshot(){
+save('filter.png');
+}
